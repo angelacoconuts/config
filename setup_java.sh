923f6c41418ceae63da48eaee39e1ed1b0d4460b
Eva Zhong <angelacoconuts@gmail.com>
2014-02-23 22:28:45 +0800

    Update setup_java.sh

--- setup_java.sh
+++ setup_java.sh
@@ -1,12 +1,12 @@
-JAVA_HOME=/usr/local/java/jdk1.7.0_45
+JAVA_HOME=/usr/local/java/jdk1.7.0_51
 PATH=$PATH:$JAVA_HOME/bin
-JRE_HOME=/usr/local/java/jre1.7.0_45
+JRE_HOME=/usr/local/java/jre1.7.0_51
 PATH=$PATH:$JRE_HOME/bin
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/jre1.7.0_45/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/jdk1.7.0_45/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/local/java/jre1.7.0_45/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/jre1.7.0_51/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/jdk1.7.0_51/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/local/java/jre1.7.0_51/bin/javaws" 1
 
-sudo update-alternatives --set java /usr/local/java/jre1.7.0_45/bin/java
-sudo update-alternatives --set javac /usr/local/java/jdk1.7.0_45/bin/javac
-sudo update-alternatives --set javaws /usr/local/java/jre1.7.0_45/bin/javaws
+sudo update-alternatives --set java /usr/local/java/jre1.7.0_51/bin/java
+sudo update-alternatives --set javac /usr/local/java/jdk1.7.0_51/bin/javac
+sudo update-alternatives --set javaws /usr/local/java/jre1.7.0_51/bin/javaws
