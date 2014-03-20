10fcbd488766cd17f7360966770ff4609b9e600d
Eva Zhong <angelacoconuts@gmail.com>
2014-03-20 22:53:54 +0800

    Update setup_java.sh

--- setup_java.sh
+++ setup_java.sh
@@ -6,15 +6,15 @@ wget --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accep
 tar xvf jdk-7u51-linux-x64.tar.gz
 tar xvf jre-7u51-linux-x64.tar.gz
 
-export JAVA_HOME=/home/ubuntu/java/jdk1.7.0_51
+export JAVA_HOME=/home/ubuntu/jdk1.7.0_51
 export PATH=$PATH:$JAVA_HOME/bin
-export JRE_HOME=/home/ubuntu/java/jre1.7.0_51
+export JRE_HOME=/home/ubuntu/jre1.7.0_51
 export PATH=$PATH:$JRE_HOME/bin
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/home/ubuntu/java/jre1.7.0_51/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/home/ubuntu/java/jdk1.7.0_51/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/home/ubuntu/java/jre1.7.0_51/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "/home/ubuntu/jre1.7.0_51/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/home/ubuntu/jdk1.7.0_51/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/home/ubuntu/jre1.7.0_51/bin/javaws" 1
 
-sudo update-alternatives --set java /home/ubuntu/java/jre1.7.0_51/bin/java
-sudo update-alternatives --set javac /home/ubuntu/java/jdk1.7.0_51/bin/javac
-sudo update-alternatives --set javaws /home/ubuntu/java/jre1.7.0_51/bin/javaws
+sudo update-alternatives --set java /home/ubuntu/jre1.7.0_51/bin/java
+sudo update-alternatives --set javac /home/ubuntu/jdk1.7.0_51/bin/javac
+sudo update-alternatives --set javaws /home/ubuntu/jre1.7.0_51/bin/javaws
