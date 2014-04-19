638c4b007a3ace1a1564320eb78ae7f03d32fef4
Eva Zhong <angelacoconuts@gmail.com>
2014-04-19 09:36:36 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,7 +72,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/angelacoconuts/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
